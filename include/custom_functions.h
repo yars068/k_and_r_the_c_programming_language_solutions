b3b3ac8a5d540d4fa5508d6da28789de3b597183
Yaroslav Silyuev <yars068@yandex.ru>
2023-12-03 21:18:27 +0300

    3-rd party functions.h: added new functions

--- include/custom_functions.h
+++ include/custom_functions.h
@@ -1,17 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <ctype.h>
+#include <errno.h>
+
+void reverse(char s[], int length) {
+  char buf[length];
+  int i, j;
+  i = 0;
+  j = length - 2; // because string is in 'hello\n\0' format
+  while (i <= length - 2 && j >= 0) {
+    buf[i] = s[j];
+    ++i;
+    --j;
+  }
+  buf[length - 1] = '\n';
+  buf[length] = '\0';
+  for (i = 0; i < length; ++i) s[i] = buf[i];
+}
+
 int chartodig(char buf[], int len) {
   int mul = 1;
   int i, res = 0;
 
   for (i = (len - 2); i >= 0; i--) { // because buf[len] = '\n'
-    res += (buf[i] - '0') * mul;
-    mul *= 10;
+    if (buf[i] >= '0' && buf[i] <= '9') {
+      res += (buf[i] - '0') * mul;
+      mul *= 10;
+    }
+    else return EINVAL;
   }
   return res;
 }
 
 int get_line(unsigned char buf[], unsigned int lim, const char msg[]) {
   unsigned int i = 0;
-  unsigned int c = '\0';
+  unsigned short int c = '\0';
 
   printf("%s ", msg);
   while ((i < lim - 1) == ((c = getchar()) != EOF) == (c != '\n'))
@@ -21,5 +45,32 @@ int get_line(unsigned char buf[], unsigned int lim, const char msg[]) {
 
   buf[i] = '\0';
   if (i > 1) return i;
-  else return 0;
+  else return EINVAL;
+}
+
+long int htol(unsigned char buf[]) {
+  long int ret = 0;
+  long int i;
+  unsigned short c;
+  int len = 0;
+  int mul = 1; /* 16^0 = 1 */
+
+  while (!isspace(buf[len++])); /* count the length */
+  for (i = len; i >= 0; --i) {
+    c = tolower(buf[i]);
+    printf("c= %c (%d)\n", c, c);
+    if (isxdigit(c)) {
+
+      if (isdigit(c))
+        ret += (c - '0') * mul;
+
+      else if (isxdigit(c))
+        ret += ((c - 'a') + 10) * mul;
+
+      mul *= 16;
+    }
+    else if (i >= 0) continue;
+    else return EINVAL;
+  }
+  return ret;
 }
