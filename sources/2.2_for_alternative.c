1f935c18c1259817537f5071272cb4eefaa9bc79
Yaroslav Silyuev <yars068@yandex.ru>
2023-12-03 21:44:41 +0300

    Added lesons 2.1, 2.2 and 2.3

---
+++ sources/2.2_for_alternative.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <limits.h>
+#define BUFSIZE 1024U
+
+int get_line(unsigned char buf[], unsigned int lim, const char msg[]) {
+  unsigned int i = 0;
+  unsigned int c = '\0';
+
+  printf("%s ", msg);
+  while ((i < lim - 1) == ((c = getchar()) != EOF) == (c != '\n'))
+    buf[i++] = c;
+
+  if (c == '\n') buf[i++] = '\n';
+
+  buf[i] = '\0';
+  if (i > 1) return i;
+  else return 0;
+}
+
+int main(void) {
+  unsigned char buf[BUFSIZE] = {'\0'};
+  unsigned int len = 0;
+  unsigned const char message[] = "Type something:";
+  
+  while ((len = get_line(buf, BUFSIZE, message)) > 0) {
+    printf("Your input is %s\n", buf);
+  }
+  return 0;
+}
---
+++ sources/2.3_htol.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <ctype.h>
+#include <errno.h>
+#include "../include/custom_functions.h"
+
+#define BUFSIZE 1024U
+
+/* Lesson 2.3. Write function atol() which fransform a hexadecimal */
+/* numbers fo an integer */
+
+int main(void) {
+  unsigned char buf[BUFSIZE] = { '\0' };
+  int len;
+  long int res = 0;
+  const unsigned char message[] = "Input hexadecimal number: ";
+
+  while ((len = get_line(buf, BUFSIZE, message)) != EINVAL) {
+    if ((res = htol(buf)) != EINVAL)
+      printf("decimal number is %d\n", res);
+    else printf("Illegal input. Try again.\n");
+  }
+  return EXIT_SUCCESS;
+}
