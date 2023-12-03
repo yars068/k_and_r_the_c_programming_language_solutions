a35e962e57749644d80752df270f0d947e28a6c1
Yaroslav Silyuev <yars068@yandex.ru>
2023-12-03 14:33:42 +0300

    3-rd party functions: added get_line()

--- include/custom_functions.h
+++ include/custom_functions.h
@@ -8,3 +8,18 @@ int chartodig(char buf[], int len) {
   }
   return res;
 }
+
+int get_line(unsigned char buf[], unsigned int lim, const char msg[]) {
+  unsigned int i = 0;
+  unsigned int c = '\0';
+
+  printf("%s ", msg);
+  while ((i < lim - 1) == ((c = getchar()) != EOF) == (c != '\n'))
+    buf[i++] = c;
+
+  if (c == '\n') buf[i++] = '\n';
+
+  buf[i] = '\0';
+  if (i > 1) return i;
+  else return 0;
+}
