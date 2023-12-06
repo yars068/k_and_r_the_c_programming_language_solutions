3059979263b03bc415cb87ddbb7fccccf73a7430
Yaroslav Silyuev <yars068@yandex.ru>
2023-12-06 20:20:51 +0300

    lesson 1.24: Debugged parts of logic: parse delimiters and preprocessor directives, except a named constansts and fixed unneeded commentary

--- sources/1.24_syntax.c
+++ sources/1.24_syntax.c
@@ -4,9 +4,23 @@
 /* unpaired brackets, quotes, broken escape sequences and comments 
 /* and so on. */
 
-#define BUFSIZE 1024
-#define OFFSET 16
-#define FALSE -1
+#define BUFSIZE      1024
+#define FIELDWIDTH     16
+#define FALSE          -1
+#define PREPROC_COUNT  13
+#define KEYWORDS_COUNT 43
+#define KEYWORD_MAXLEN 14
+
+/* Map definitions for non-keywords and other lexems */
+
+#define OTHER                      8
+#define LVAL                       1
+#define SINGLE_STRING_COMMENT      2
+#define MULTI_STRING_COMMENT_BEGIN 3
+#define MULTI_STRING_COMMENT_END   4
+#define DELIMITER                  5
+#define PREPROCESSOR               6
+#define KEYWORD                    7
 
 int get_line(char buf[], int lim, char msg[]) {
   int c, len;
@@ -22,14 +36,14 @@ int get_line(char buf[], int lim, char msg[]) {
   return len;
 }
 
-int get_keyword(char buf[], char res[], int number, int offset) {
+int get_keyword(char buf[], char res[], int number, int field_width) {
   int i = 0;
   int len = 0;
   int ret = FALSE;
-  int first_item = (number * offset) + 1;
+  int first_item = (number * field_width) + 1;
   
-  len = buf[number * offset];
-  if (len >= 0) {
+  len = buf[number * field_width];
+  if (len > 0) {
     while ((buf[first_item] != 0) && (i < len)) {
       res[i] = buf[first_item + i];
       ++i;
@@ -41,9 +55,13 @@ int get_keyword(char buf[], char res[], int number, int offset) {
 }
 
 int main(void) {
-  int i, kw_len, len, number;
+  int i, j, k, len;
+  int kw_len = FALSE;
+  int word_len = 0;
+  char word[BUFSIZE] = {'\0'};
   char keyword[BUFSIZE] = {'\0'};
   char buf[BUFSIZE] = {'\0'};
+  int map[BUFSIZE] = {FALSE};
   char keywords[] = { 6, 's', 'i', 'z', 'e', 'o', 'f', '0', '0', '0', '0', '0', '0', '0', '0', '0', 
                       7, 't', 'y', 'p', 'e', 'd', 'e', 'f', '0', '0', '0', '0', '0', '0', '0', '0', 
                       4, 'a', 'u', 't', 'o', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 
@@ -90,20 +108,52 @@ int main(void) {
                      13, '_', 'T', 'h', 'r', 'e', 'a', 'd', '_', 'l', 'o', 'c', 'a', 'l', '0', '0'
                      };
 
-  char delimiters[] = {',', ';', '.', '+', '-', '*', '^', '&', '=', '~', '!', '/', '<', '>', '(', 
-                       ')', '{', '}', '[', ']', '|', '%', '?', '\'', '"', ':', '_', '\\', '#'
+/* Place them itself in a map. Possibly better is use named constants instead of this, because */
+/* it would be more consistent. */
+  char delimiters[] = {' ', ',', ';', '.', '+', '-', '*', '^', '&', '=', '~', '!', '/', '<', '>', '(',
+                       ')', '{', '}', '[', ']', '|', '%', '?', '\'', '"', ':', '_', '\\', '#', '\0',
                       };
 
-  char preproc[] = { 6, 'd', 'e', 'f', 'i', 'n', 'e', '0', '0', '0', '0', '0', '0', '0', '0', '0',
-                     7, 'i', 'n', 'c', 'l', 'u', 'd', 'e', '0', '0', '0', '0', '0', '0', '0', '0',
-                     6, 'p', 'r', 'a', 'g', 'm', 'a', '0', '0', '0', '0', '0', '0', '0', '0', '0'
+  char preproc[] = {  6, 'd', 'e', 'f', 'i', 'n', 'e', '0', '0', '0', '0', '0', '0', '0', '0', '0',
+                      5, 'u', 'n', 'd', 'e', 'f', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
+                      7, 'i', 'n', 'c', 'l', 'u', 'd', 'e', '0', '0', '0', '0', '0', '0', '0', '0',
+                      2, 'i', 'f', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
+                      5, 'i', 'f', 'd', 'e', 'f', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
+                      6, 'i', 'f', 'n', 'd', 'e', 'f', '0', '0', '0', '0', '0', '0', '0', '0', '0',
+                      4, 'e', 'l', 's', 'e', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
+                      4, 'e', 'l', 'i', 'f', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
+                      5, 'e', 'n', 'd', 'i', 'f', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
+                      4, 'l', 'i', 'n', 'e', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
+                      5, 'e', 'r', 'r', 'o', 'r', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
+                      7, 'w', 'a', 'r', 'n', 'i', 'n', 'g', '0', '0', '0', '0', '0', '0', '0', '0',
+                      6, 'p', 'r', 'a', 'g', 'm', 'a', '0', '0', '0', '0', '0', '0', '0', '0', '0',
                    };
-  
-  while ((len = get_line(buf, BUFSIZE, "Enter code to check: ")) > 0) {
-    i = 0;
-    while (buf[i] != '\n') {
-      
+
+  while ((len = get_line(buf, BUFSIZE, "Enter the source code to check: ")) > 0) {
+    /* Start with delimiters */
+    for (i = 0; buf[i] != '\0'; ++i) {
+      j = 0;
+      while (delimiters[j] != buf[i] && delimiters[j] != '\0') j++;
+      if (delimiters[j] == buf[i]) {
+        map[i] = DELIMITER;
+      }
+      else map[i] = OTHER;
     }
+    /* Then, prepocessor directives: */
+    /* Preprocessor directive always starts with '#' at begin of string */
+    if (buf[0] == '#') {
+      for (i = 1; map[i] != DELIMITER; ++i) word[word_len++] = buf[i]; /* get a word */
+
+      for (i = 0; i < KEYWORDS_COUNT && ((kw_len = get_keyword(preproc, keyword, i, FIELDWIDTH)) != FALSE); ++i) {
+        for (j = 0; j < kw_len && word[j] == keyword[j]; j++) map[j + 1] = PREPROCESSOR;
+        for (j = 0; j < kw_len; ++j) keyword[j] = '\0'; /* reset after when all done */
+      }
+    }
+
+    printf("debug: you entered a string:\n%s", buf);
+    printf("text mapped as follows:\n");
+    for (i = 0; (map[i] > 0) && (map[i] != FALSE); i++) printf("%u", map[i]);
+    putchar('\n');
   }
   return 0;
 }
