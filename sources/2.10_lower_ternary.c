1ceb7a4e84a3940d9039a591df4e3a577067e329
Yaroslav Silyuev <yars068@yandex.ru>
2023-12-12 18:45:45 +0300

    Added 2.10_lower_ternary.c source

---
+++ sources/2.10_lower_ternary.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+char lower(char s) {
+  return (s >= 'A' && s <= 'Z') ? ((s - 'A') + 'a') : (s >= 'a' && s <= 'z') ? s : '\0';
+}
+
+int main(void) {
+  char w[] = "aBcDeF";
+  int i = 0;
+  
+  printf("Source: %s, transformed: ", w);
+  for (i = 0; w[i] != '\0'; i++) {
+    printf("%c", lower(w[i]));
+  }
+  putchar('\n');
+  return 0;
+}
+
