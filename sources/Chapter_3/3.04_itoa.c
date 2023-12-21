66c99d99afb992ccdcb37d2e8292ef32d234a040
Yaroslav Silyuev <yars068@yandex.ru>
2023-12-22 00:04:11 +0300

    Chapter 3: added 3.04_itoa.c source

---
+++ sources/Chapter_3/3.04_itoa.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <limits.h>
+#define BUFSIZE 25
+#define YES 1
+#define NO  0
+
+/* Lesson 3.4. Explain why this function cannot handle the greatest negative */
+/* number? The answer: when comparing n < 0 to determine the sign, becomes a type owerflow. */
+
+/* reverse: replace the s with s reverted */
+void reverse(char s[], int length) {
+  char buf[BUFSIZE];
+  int i, j;
+  i = 0;
+  j = length;
+
+  while (j >= 0) {
+    if (s[j] != '\n'&& s[j] != '\0') buf[i++] = s[j];
+    --j;
+  }
+
+  for (i = 0; s[i] != '\0'; ++i) s[i] = buf[i];
+}
+
+/* itoa: transform an integer to text form */
+void itoa (int n, char s[]) {
+  int i, sign = NO;
+  int is_max = NO;
+
+  for (i = 0; s[i] != '\0'; ++i) s[i] = '\0'; /* reset buffer */
+
+  if (n > 0) sign = NO;
+  else if (n + 1 == INT_MIN + 1) {
+    sign = YES;
+    n = -(n += 1);
+    is_max = YES;
+  }
+  else {
+    sign = YES;
+    n = -n;
+  }
+  i = 0;
+  do {
+    if (i == 0 && sign && is_max) s[i++] = 1 + (n % 10) + '0';
+    else s[i++] = n % 10 + '0';
+  } while ((n /= 10) > 0);
+  
+  if (sign) s[i++] = '-';
+
+  s[i] = '\0';
+  reverse(s, i);
+}
+
+int main(void) {
+  char buf[BUFSIZE] = {'\0'};
+  int len = 0;
+  int number = -365;
+  printf("Number is: %d\n", number);
+  itoa(number, buf);
+  printf("Now how itoa() works: %s\n", buf);
+
+}
