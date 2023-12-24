f27cefacc27b06581857176003990852d4793616
Yaroslav Silyuev <yars068@yandex.ru>
2023-12-24 23:22:50 +0300

    Chapter 4: 4.03_rpncalc.c: Add support for modulus (%) operator

--- sources/Chapter_4/4.03_rpncalc.c
+++ sources/Chapter_4/4.03_rpncalc.c
@@ -3,6 +3,9 @@
 #define MAXOP 100 /* max size of operand or operator */
 #define NUMBER '0' /* signal that a number was found */
 
+/* Lesson 4.3. Modify this calculator: add the mudulus (%) */
+/* operator and provisions to negative numbers */
+
 int getop (char []);
 void push (double);
 double pop (void);
@@ -34,7 +37,14 @@ int main (void) {
           push (pop() / op2);
         else
           printf("Error: division by zero\n");
-          break;
+        break;
+      case '%':
+        op2 = pop();
+        if (op2 != 0.0)
+          push((int)pop() % (int)op2);
+        else
+          printf("Error: division by zero\n");
+        break;
       case '\n' :
         printf("\t%.8g\n", pop());
         break;
