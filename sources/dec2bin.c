609315eab9e5abd602ca774af41e2ba2d540129d
Yaroslav Silyuev <yars068@yandex.ru>
2024-01-30 20:47:24 +0300

    dec2bin.c: code fixup to reflect changes in my_functions.h

--- sources/dec2bin.c
+++ sources/dec2bin.c
@@ -1,32 +1,11 @@
 #include <stdio.h>
 #include <ctype.h>
-#include <limits.h>
-#include "../include/custom_functions.h"
 #define BUFSIZE 66 /* 8 bits per byte, 8 bytes total plus a '\n' and '\0' */ 
+#include "../include/my_functions.h"
 
-/* dec2bin: transform a decimal integer to binary form */
 
-int dtob(long divisible, unsigned char res[]) {
-const unsigned long base = 2; /* base of bynary number system */
-unsigned long remainder = 0;
-int len, n;
-
-  for (n = 0; n < BUFSIZE; n++) res[n] = '\0'; /* reset */
-
-  for (n = 0; (power(base, n) < divisible); n++);
-  if (divisible < power(base, n)) len = n - 1;
-  else len = n;
-
-  while (divisible) {
-    remainder = divisible % base;
-    divisible /= base;
-    if (remainder) res[len] = '1';
-    else res[len] = '0';
-    len--;
-  }
-
-  return len; /* 1 if success, otherwise 0 */
-}
+int get_line(char [], int, const char []);
+int dtob(long, unsigned char []);
 
 int main(void) {
   const unsigned char msg[] = "Enter decimal number:";
@@ -48,4 +27,27 @@ int main(void) {
     j = 0;
   }
   return 0;
+}
+
+/* dec2bin: transform a decimal integer to binary character form */
+int dtob(long divisible, unsigned char res[]) {
+const unsigned long base = 2; /* base of bynary number system */
+unsigned long remainder = 0;
+int len, n;
+
+  for (n = 0; n < BUFSIZE; n++) res[n] = '\0'; /* reset */
+
+  for (n = 0; (power(base, n) < divisible); n++);
+  if (divisible < power(base, n)) len = n - 1;
+  else len = n;
+
+  while (divisible) {
+    remainder = divisible % base;
+    divisible /= base;
+    if (remainder) res[len] = '1';
+    else res[len] = '0';
+    len--;
+  }
+
+  return len; /* 1 if success, otherwise 0 */
 }
