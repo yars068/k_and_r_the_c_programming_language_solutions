13e00c15ad22bd233851ff226060bfca5ed7d432
Yaroslav Silyuev <yars068@yandex.ru>
2024-02-03 11:03:32 +0300

    Chapter 5: source 5.10_expr.c, revised version

--- sources/Chapter_5/5.10_expr.c
+++ sources/Chapter_5/5.10_expr.c
@@ -1,34 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <ctype.h>
 #define MAXARGS 10
 
-/* Lesson 5.10. Write the program expr, which evaluates a reverse polish expression, where */
-/* every operator or operand is a separate command line argument */
+/* Lesson 5.10. Write the program expr, which evaluates a expression in the */
+/* reverse polish notation, where every operator or operand is a separate command line argument */
+
+/* NOTE: The shell will perform a pathname expansion, so we need to shield, */
+/* in particular, the asterisk. This done by adding a '\' before it. */
+/* Another way is placing all the parameters on the command line to a double quotes */
+/* and then process them as a regular character string. But this leads program to be */
+/* slightly complicated ;) */
 
 int main(int argc, char *argv[]) {
   const int next = 1;
-  int idx = 0;
-  int vars[MAXARGS];
+  int i = 0;
+  int vals[MAXARGS];
+  char **start;
 
   while (--argc > 0) {
-    if (isdigit(**argv) && idx < MAXARGS) *(vars + idx++) = atoi((*argv));
-    else if (idx > MAXARGS) {
+    if (isdigit(**argv) && i < MAXARGS) {
+      *(vals + i++) = atoi((*argv));
+      start = argv;
+    }
+    else if (i > MAXARGS) {
       printf("Too many arguments\n");
       return EXIT_FAILURE;
     }
     *++argv;
   }
-  if (**argv == '+') *vars += *(vars + next);
-  else if (**argv == '-') *vars -= *(vars + next);
-  else if (**argv == '*') *vars *= *(vars + next);
-  else if (**argv == '/') *vars /= *(vars + next);
-  else if (**argv == '%') *vars %= *(vars + next);
-  else {
-    printf("Unknown operator\n");
-    return EXIT_FAILURE;
+  i = 1;
+  while (argv != start) {
+    if (**argv == '+') *vals += *(vals + i);
+    else if (**argv == '-') *vals -= *(vals + i);
+    else if (**argv == '*') *vals *= *(vals + i);
+    else if (**argv == '/') *vals /= *(vals + i);
+    else if (**argv == '%') *vals %= *(vals + i);
+    else {
+      printf("Unknown operator\n");
+      return EXIT_FAILURE;
+    }
+    *--argv;
+    i++;
   }
-  printf("Result: %d\n", *vars);
+  printf("Result: %d\n", *vals);
   return EXIT_SUCCESS;
 }
