e7a7898ee31f2566652bb6fab9da99007e75c275
Yaroslav Silyuev <yars068@yandex.ru>
2025-09-07 00:11:57 +0300

    Chapter_5: new source 5.12_tabman.c added

---
+++ sources/Chapter_5/5.12_tabman.c
@@ -0,0 +1,221 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+#define TABSIZE 8
+#define BUFSIZE 1024
+#define MAXARGS 10
+#define NO_ARGS 1
+
+/* Lesson 5.11. Extend programs 'detab' and 'entab' to accept the list of tab stops */
+/* as a command line arguments. Use the default tab stop size if there are no arguments. */
+/* tabman -- a tab and space manipulation program */
+
+int ask_line(char *buf, int lim, char *msg);
+int get_line(char *buf, int lim);
+int detab(char *buf, int tabsize, int tab_start_pos);
+int entab(char *buf, int tabsize, int tab_start_pos);
+
+int main(int argc, char *argv[]) {
+  enum booleans { FALSE, TRUE };
+  enum ops { DETAB, ENTAB };
+  char *msg = "Enter text to operate:\n";
+  char buf[BUFSIZE] = { '\0' };
+  int stops_arr[MAXARGS] = { 0 };
+  int *stops = stops_arr;
+  int mode = FALSE;
+  int def_tab_size = TRUE;  /* use default tab stop size */
+  int def_tab_start = TRUE; /* tab stop starts from beginnig of line */
+  int tab_start_pos = 0;
+  int flag = FALSE;         /* common use flag */
+  int tabsize = TABSIZE;
+
+  if (argc > MAXARGS) {
+    printf("Error: too many arguments\n");
+    return EXIT_FAILURE;
+  }
+
+  if (argc == NO_ARGS) {
+    printf("Usage:\ntabman [-d|-e] [-number]... \ntabman [-d|-e] [-number] [+number]\n");
+    return EXIT_SUCCESS;
+  }
+
+  while (argc-- != NO_ARGS && *++argv) { /* Skip *argv[0] because this points to program name */
+    while (**argv == '-' || **argv == '+') {
+      if (**argv == '+') {
+        def_tab_start = FALSE;
+        if (argc != NO_ARGS) {
+          printf("tabman: Error: too many arguments\n");
+          return EXIT_FAILURE;
+        }
+      }
+      switch (*++*argv) {
+        case 'd':
+          mode = DETAB;
+          break;
+        case 'e':
+          mode = ENTAB;
+          break;
+        default:
+          if (isdigit(**argv)) {
+            *stops++ = atoi(*argv);
+            def_tab_size = FALSE;
+          }
+          break;
+      }
+    }
+  }
+
+  stops = stops_arr;
+  if (!def_tab_size && !def_tab_start) {
+    tab_start_pos = *stops++;
+    tabsize = *stops;
+    printf("tabman: tab stop first position is %d, tab stop size is %d\n", tab_start_pos, tabsize);
+  }
+
+  /* determine the tab stop size */
+  if (!def_tab_size && def_tab_start) {
+    stops = stops_arr;
+    tabsize = *++stops - *(stops - 1);
+  }
+
+  /* check tabstop size for equality */
+  stops = stops_arr;
+  flag = FALSE;
+  while (!def_tab_size && def_tab_start && *++stops) {
+    if (*stops - *(stops - 1) == tabsize) flag = TRUE;
+    else flag = FALSE;
+  }
+  if (!flag && !def_tab_size && def_tab_start) {
+    printf("Error: tab stops should be equal\n");
+    return EXIT_FAILURE;
+  }
+
+  /* check work mode and run it */
+  flag = FALSE;
+  printf("Entering %s mode\n", (mode ? "entab" : "detab"));
+  if (ask_line(buf, BUFSIZE, msg)) {
+    switch (mode) {
+      case DETAB:
+        if (detab(buf, tabsize, tab_start_pos)) flag = TRUE;
+        break;
+      case ENTAB:
+        if (entab(buf, tabsize, tab_start_pos)) flag = TRUE;
+        break;
+    }
+  }
+
+  if (flag) {
+    printf("Result:\n%s\n", buf);
+    return EXIT_SUCCESS;
+  }
+  else {
+    printf("tabman: error: unable to %s\n", (mode ? "entab" : "detab"));
+    return EXIT_FAILURE;
+  }
+}
+
+#define TAB '\t'
+#define SPACE ' '
+
+static int eval_space_count(char *buf, char *start, int tabsize);
+
+/* detab: replace a tabstops by proper count of spaces */
+int detab(char *buf, int tabsize, int tab_start_pos) {
+  char tmpbuf[BUFSIZE] = { '\0' };
+  char *tmp = tmpbuf;
+  char *start = buf;
+  int len = 0;
+
+  /* Save input buffer to a local buffer, and clear it */
+  for (int i = 0; *(tmp + i) = *(buf + i); i++) 
+    *(buf + i) = '\0';
+
+  while (*tmp) {
+    /* keep input data unchanged before tab_start_pos */
+    if (*tmp != TAB || ((tmp - tmpbuf) <= tab_start_pos)) {
+      *buf++ = *tmp++;
+      len++;
+    }
+    else {
+      int spaces = eval_space_count(buf, start, tabsize);
+      while (spaces--) {
+        *buf++ = SPACE;
+        len++;
+      }
+      *tmp++; /* skip current position, because here is a tabstop */
+    }
+  }
+  return len;
+}
+
+static int get_space_count(char *buf);
+
+/* entab: replace sequences of spaces by proper number of tabstops and spaces, */
+/* keeping input looks unchanged */
+int entab(char *buf, int tabsize, int tab_start_pos) {
+  char tmpbuf[BUFSIZE] = { '\0' };
+  char *tmp = tmpbuf;
+
+  /* First, copy input buffer to local buffer and clear input buffer */
+  for (int i = 0; *(tmp + i) = *(buf + i); i++) *(buf + i) = '\0';
+
+  /* Then, entab it */
+  char *start = tmp;
+  while (*tmp) {
+    /* keep input data unchanged before tab_start_pos */
+    if (*tmp != SPACE || ((tmp - start) <= tab_start_pos)) *buf++ = *tmp++;
+    else {
+      int spaces = get_space_count(tmp);
+      int spaces_to_next_tab = eval_space_count(tmp, start, tabsize);
+      if (spaces == spaces_to_next_tab) {
+        *buf++ = TAB;
+        tmp += spaces;
+      }
+      else if (spaces > spaces_to_next_tab && spaces_to_next_tab) {
+        *buf++ = TAB;
+        tmp += spaces;
+        while (spaces != spaces_to_next_tab) {
+          *buf++ = SPACE;
+          spaces--;
+        }
+      }
+      else {
+        *buf++ = SPACE;
+        tmp++;
+      }
+    }
+  }
+
+  return tmp - start;
+}
+
+/* get_count_spaces: return count of spaces to first non-space character */
+static int get_space_count(char *buf) {
+  int i = 0;
+  while (*buf++ == SPACE) i++;
+
+  return i;
+}
+
+/* eval_space_count: returns the number of spaces needed to fill the current tabstop */
+static int eval_space_count(char *buf, char *start, int tabsize) {
+  return (buf - start) ? (tabsize - (buf - start) % tabsize) % tabsize : tabsize;
+}
+
+/* get_line: store the input stream to array */
+int get_line(char *buf, int lim) {
+  char *tmp = buf;
+
+  while (lim-- && (*tmp = getchar()) != EOF && *tmp++ != '\n');
+  *tmp++ = '\0';
+  return tmp - buf;
+}
+
+/* ask_line: ask the question to user */
+int ask_line(char *buf, int lim, char *msg) {
+
+  printf("%s", msg);
+  return get_line(buf, lim);
+}
+
