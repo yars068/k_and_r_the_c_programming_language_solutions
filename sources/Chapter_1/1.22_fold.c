db80b9449a40801d2b07a7172a9aa20a368302a5
Yaroslav Silyuev <yars068@yandex.ru>
2025-09-08 02:04:10 +0300

    Chapter_1: 1.22_fold.c: version with a function

--- sources/Chapter_1/1.22_fold.c
+++ sources/Chapter_1/1.22_fold.c
@@ -1,43 +1,76 @@
 #include <stdio.h>
-#define BUFSIZE 1000
-#define MAXLEN 20
 
-/* Write a program to "fold" the lines longer than n symbols into two or
-more shorter lines */
+#define BUFSIZE 1024
+#define FOLDLEN 72
 
-int get_line(char s[], int lim) {
-  int c, len;
-  c = len = 0;
+/* Lesson 1.22. Write a program to "fold" the lines longer than n (80 by default) */
+/* symbols into two or more shorter lines */
+
+int fold(char buf[], int bufsize, int len, int fold_len);
+int get_line(char buf[], int lim);
+
+int main(void) {
+  char buf[BUFSIZE];
+  int len = 0;
+  int pos = 0;
+
+  /* initialize array buf */
+  for (pos = 0; pos < BUFSIZE; ++pos) buf[pos] = '\0';
+
   printf("Type something:\n");
+  while ((len = get_line(buf, BUFSIZE)) > 0) {
+    if (fold(buf, BUFSIZE, len, FOLDLEN) > 0) {
+      printf("\nFolded text:\n%s\n", buf);
+      return 0; /* Success. Operating system expects 0 in this case, otherwise -- non-null */
+    }
+    else {
+      printf("Can\'t fold\n");
+      return 1;
+    }
+  }
+  return 0;
+}
+
+int fold(char buf[], int bufsize, int len, int fold_len) {
+  char tmp[BUFSIZE] = { '\0' };
+  int bi = 0; /* index for buf */
+  int ti = 0; /* index for tmp */
+
+  if (len <= fold_len) return 0; /* too small line */
+
+  /* copy the part of line before buf[maxlen] */
+  while (ti <= fold_len && buf[bi] != '\0') tmp[ti++] = buf[bi++];
+
+  /* then, find position to fold before maxlen */
+  while (ti != 0 && tmp[ti] != ' ' && tmp[ti] != '\t') {
+    ti--;
+  }
+
+  if (ti == 0) return 0; /* we returned to begiinig of line, cannot fold */
+  else {
+    tmp[ti++] = '\n'; /* insert a newline, then go to next element of array */
+    bi = ti; /* copy the rest of line */
+    while (bi < bufsize && buf[bi] != '\0') tmp[ti++] = buf[bi++];
+  }
+
+  /* Now, clear the input buffer, then copy the result from temporary buffer */
+  for (ti = 0; ti < bufsize && tmp[ti] != '\0'; ti++)
+    buf[ti] = tmp[ti];
+
+  buf[ti] = '\0';
+
+  return bufsize - ti; /* Inside the C program, non-null means true, 0 -- false */
+}
+
+/* get_line: store a string to input buffer */
+int get_line(char s[], int lim) {
+  int c; /* no sense to initialize this variable, because it will be overwritten */
+  int len = 0;
+
   while (len < lim - 1 && (c = getchar()) != EOF && c != '\n')
     s[len++] = c;
 
   if (c == '\n') s[len++] = c;
   s[len] = '\0';
   return len;
-}
-
-int main(void) {
-  char buf[BUFSIZE];
-  int len, pos;
-  len = pos = 0;
-  for (pos = 0; pos < BUFSIZE; ++pos) buf[pos] = 0;
-
-  while ((len = get_line(buf, BUFSIZE)) > 0) {
-    if (len >= BUFSIZE - 1) {
-      printf("\nToo long line\n");
-      break;
-    }
-    if (len > MAXLEN) {
-      for (pos = MAXLEN; pos >= 0; --pos) {
-        if (buf[pos] == ' ' || buf[pos] == '\t') break;
-        else if (pos == 0) {
-          printf("\nCan\'t fold this line\n");
-          break;
-        }
-      }
-      if (buf[pos - 1] != ' ' || buf[pos - 1] != '\t') buf[pos] = '\n';
-    }
-    printf("Result:\n%s\n", buf);
-  }
 }
